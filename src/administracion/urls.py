30bdf0ad8092e7f147f00447941cb0cb0f3a73e7
Benjamin Ruiz-Tagle Valderrama <benjamin@MacBook-Pro-de-Benjamin.local>
2014-12-05 18:33:25 -0300

    app administracion

---
+++ src/administracion/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from .views import *
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$',Home(0).inicio),
+    url(r'^admin/', include(admin.site.urls)),
+) 
+
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
