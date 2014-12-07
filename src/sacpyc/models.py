3e17c123683ada0f5eb78237ec53bd0551a64988
arielMerino <ariel.merino@usach.cl>
2014-12-07 14:51:43 -0300

    item y tipoItem listo

--- src/sacpyc/models.py
+++ src/sacpyc/models.py
@@ -51,74 +51,6 @@ class Auditoria(models.Model):
         db_table = 'auditoria'
 
 
-class AuthGroup(models.Model):
-    id = models.IntegerField(primary_key=True)  # AutoField?
-    name = models.CharField(unique=True, max_length=80)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group'
-
-
-class AuthGroupPermissions(models.Model):
-    id = models.IntegerField(primary_key=True)  # AutoField?
-    group = models.ForeignKey(AuthGroup)
-    permission = models.ForeignKey('AuthPermission')
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group_permissions'
-
-
-class AuthPermission(models.Model):
-    id = models.IntegerField(primary_key=True)  # AutoField?
-    name = models.CharField(max_length=50)
-    content_type = models.ForeignKey('DjangoContentType')
-    codename = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_permission'
-
-
-class AuthUser(models.Model):
-    id = models.IntegerField(primary_key=True)  # AutoField?
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField()
-    is_superuser = models.IntegerField()
-    username = models.CharField(unique=True, max_length=30)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.CharField(max_length=75)
-    is_staff = models.IntegerField()
-    is_active = models.IntegerField()
-    date_joined = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user'
-
-
-class AuthUserGroups(models.Model):
-    id = models.IntegerField(primary_key=True)  # AutoField?
-    user = models.ForeignKey(AuthUser)
-    group = models.ForeignKey(AuthGroup)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_groups'
-
-
-class AuthUserUserPermissions(models.Model):
-    id = models.IntegerField(primary_key=True)  # AutoField?
-    user = models.ForeignKey(AuthUser)
-    permission = models.ForeignKey(AuthPermission)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_user_permissions'
-
-
 class CantidadHistorica(models.Model):
     cantidad = models.IntegerField(db_column='CANTIDAD', blank=True, null=True)  # Field name made lowercase.
     numero_personas = models.IntegerField(db_column='NUMERO_PERSONAS', blank=True, null=True)  # Field name made lowercase.
@@ -200,53 +132,6 @@ class Cotizacion(models.Model):
         db_table = 'cotizacion'
 
 
-class DjangoAdminLog(models.Model):
-    id = models.IntegerField(primary_key=True)  # AutoField?
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.IntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', blank=True, null=True)
-    user = models.ForeignKey(AuthUser)
-
-    class Meta:
-        managed = False
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    id = models.IntegerField(primary_key=True)  # AutoField?
-    name = models.CharField(max_length=100)
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'django_content_type'
-
-
-class DjangoMigrations(models.Model):
-    id = models.IntegerField(primary_key=True)  # AutoField?
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_session'
-
-
 class Garzon(models.Model):
     mail_garzon = models.CharField(db_column='MAIL_GARZON', max_length=45, blank=True)  # Field name made lowercase.
     telefono_garzon = models.IntegerField(db_column='TELEFONO_GARZON', blank=True, null=True)  # Field name made lowercase.
@@ -282,8 +167,8 @@ class Ingrediente(models.Model):
 
 
 class IngredienteItem(models.Model):
-    cantidad_ingrediente_especial = models.IntegerField(db_column='CANTIDAD_INGREDIENTE_ESPECIAL', blank=True, null=True)  # Field name made lowercase.
-    unidad_ingrediente_especial = models.CharField(db_column='UNIDAD_INGREDIENTE_ESPECIAL', max_length=10, blank=True)  # Field name made lowercase.
+    cantidad_ingrediente = models.IntegerField(db_column='CANTIDAD_INGREDIENTE', blank=True, null=True)  # Field name made lowercase.
+    unidad_ingrediente = models.CharField(db_column='UNIDAD_INGREDIENTE', max_length=10, blank=True)  # Field name made lowercase.
     idingredienteitem = models.IntegerField(db_column='IDINGREDIENTEITEM', primary_key=True)  # Field name made lowercase.
     idingrediente = models.ForeignKey(Ingrediente, db_column='IDINGREDIENTE')  # Field name made lowercase.
     iditem = models.ForeignKey('Item', db_column='IDITEM')  # Field name made lowercase.
